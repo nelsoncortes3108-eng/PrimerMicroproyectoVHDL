1bba05b3466e86813a13c31d2e81462c7d2c89a3
nelsoncortes3108-eng <nelson.cortes3108@gmail.com>
2026-09-20 20:30:20 -0500

    Cree otro divisor
    Este tambien luego lo juntare en el seleccionador

---
+++ TEMPORIZADOR/divisordefrecuencia2/divisordefrecuencia2.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity divisordefrecuencia2 is
+    port (
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        clk_out : out std_logic
+    );
+end entity divisordefrecuencia2;
+
+architecture arch_divisordefrecuencia2 of divisordefrecuencia2 is
+    signal count    : integer range 0 to 12500000 := 0;
+    signal temporal : std_logic := '0';
+begin
+    process (clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                count    <= 0;
+                temporal <= '0';
+            elsif (count = 12499999) then
+                temporal <= not temporal;
+                count    <= 0;
+            else
+                count <= count + 1;
+            end if;
+        end if;
+    end process;
+
+    clk_out <= temporal;
+end architecture arch_divisordefrecuencia2;
